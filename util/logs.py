1a4eebf66e6912fb286474b2d5fd3c95ee60e6eb
Aeolus0 <dhash+git@outlook.com>
2014-06-16 20:01:38 +0530

    Add SocketIO.js to perform live updating of the stats and logs page

--- util/logs.py
+++ util/logs.py
@@ -1,5 +1,16 @@
 __author__ = 'dhash'
 def gh_log(logfile):
     import commands
+    #TODO sanitize logfile, as stands allows for arbitrary code injection
     last_100 = commands.getoutput('tail -n 100 ' + logfile)
     return last_100
+
+def view_queue():
+    import commands
+    current_queue = commands.getoutput('greyhole --view-queue')
+    return current_queue
+
+def iostat():
+    import commands
+    return commands.getoutput('greyhole --iostat')
+
