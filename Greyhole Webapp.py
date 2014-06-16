19f3c10551ba59f671b12bd75ea334cb13671cb1
Aeolus0 <dhash+git@outlook.com>
2014-06-16 20:00:43 +0530

    Add instructions on how to format the SocketIO data

--- Greyhole Webapp.py
+++ Greyhole Webapp.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+from flask.ext.socketio import SocketIO, emit
 from util import *
+#TODO sqlite3 db connection to store sudo pw, first time run, log locations etc
+import sqlite3
 
 app = Flask(__name__)
 
+#Start SocketIO event handlers
+@SocketIO.on('r_logs')
+def get_logs():
+    emit('logs_resp', {'data': logs.gh_log})
+
+#End SocketIO event handlers
 
 @app.route('/')
-def hello_world():
+def main():
+
     return render_template("Main.html", user='Dhash')
 
 """@app.route('/disks')
