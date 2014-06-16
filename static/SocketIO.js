6a58d04beb0ebf949c547756afcfe4c14f4531af
Aeolus0 <dhash+git@outlook.com>
2014-06-16 20:15:50 +0530

    SocketIO endpoint grabbers added

---
+++ static/SocketIO.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/stats_and_logs');
+    socket.on('logs_resp', function(msg) {
+        $('#logs_resp').append('<p>Received: ' + msg.data + '</p>');
+    });
+    socket.on('work_queue_resp', function(msg) {
+        $('#work_queue_resp').append('<p>Received: ' + msg.data + '</p>');
+    });
+    socket.on('iostat_resp', function(msg) {
+        $('#iostat_resp').append('<p>Received: ' + msg.data + '</p>');
+    });
+    socket.on('raw_usage_resp', function(msg) {
+        $('#raw_usage_resp').append('<p>Received: ' + msg.data + '</p>');
+    });
+    $('form#emit').submit(function(event) {
+        socket.emit('my event', {data: $('#emit_data').val()});
+        return false;
+    });
+    $('form#broadcast').submit(function(event) {
+        socket.emit('my broadcast event', {data: $('#broadcast_data').val()});
+        return false;
+    // do a Jquery while true delay 10 loop to keep hitting r_logs and such
+    });
+});
