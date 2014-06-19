ded9b3ddebf8f2009b87c5e1317ce5daf2cf3e42
Aeolus0 <dhash+git@outlook.com>
2014-06-19 21:21:12 +1000

    adding db framework code, and start adding first time setup code

--- Greyhole Webapp.py
+++ Greyhole Webapp.py
@@ -3,6 +3,8 @@ from flask.ext.socketio import SocketIO, emit
 from util import *
 #TODO sqlite3 db connection to store sudo pw, first time run, log locations etc
 import sqlite3
+import os.path
+
 
 app = Flask(__name__)
 
@@ -15,11 +17,24 @@ def get_logs():
 
 @app.route('/')
 def main():
+    if os.path.isfile('config.db'):
+        db = sqlite3.connect('config.db')
+        cursor = db.cursor()
+        cursor.execute('''SELECT pkey, user, sudo_pw, gh_log_loc FROM conf ''')
+    else:
+        return redirect('/first_time_setup')
+
 
     return render_template("Main.html", user='Dhash')
 
 """@app.route('/disks')
 def charts():"""
+
+@app.route('/first_time_setup')
+def setup():
+    open('config.db', 'w+')
+    db = sqlite3.connect('config.db')
+    #TODO add forms to get GH log path, sudo_pw, username etc
 
 
 @app.route('/stats_and_logs')
